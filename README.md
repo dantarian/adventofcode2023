60b280060ced0b04c7e58dd91f9d6d5410423f9f
Warren Jones <warren@pencethren.org>
2023-11-30 23:04:01 +0000

    chore: initialise for 2023

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Advent of Code 2022
+# Advent of Code 2023
 
-My solutions to the Advent of Code 2022, using Elixir.
+My solutions to the Advent of Code 2023, using Elixir.
 
 To use, run `iex -S mix` from the `aoc` folder, then at the prompt, for example, `Day1.part1("inputs/day1.txt")`.
--- aoc/lib/day1.ex
+++ aoc/lib/day1.ex
@@ -1,24 +1,13 @@
 defmodule Day1 do
   def part1(file) do
-    file |> File.read!() |> String.split("\n") |> max_elf({0, 0})
+    file |> File.read!() |> String.split("\n") |> do_something()
   end
 
-  defp max_elf(list, {mx, curr}) when list == [], do: max(mx, curr)
-  defp max_elf([head | tail], {mx, curr}) when head == "", do: max_elf(tail, {max(mx, curr), 0})
-  defp max_elf([head | tail], {mx, curr}), do: max_elf(tail, {mx, curr + String.to_integer(head)})
+  defp do_something(list), do: list
 
   def part2(file) do
-    file |> File.read!() |> String.split("\n") |> max_three({[0, 0, 0], 0})
+    file |> File.read!() |> String.split("\n") |> do_something_else()
   end
 
-  defp replace_least_if_greater(list, new), do: [new | list] |> Enum.sort() |> tl
-
-  defp max_three(list, {mx, curr}) when list == [],
-    do: Enum.sum(replace_least_if_greater(mx, curr))
-
-  defp max_three([head | tail], {mx, curr}) when head == "",
-    do: max_three(tail, {replace_least_if_greater(mx, curr), 0})
-
-  defp max_three([head | tail], {mx, curr}),
-    do: max_three(tail, {mx, curr + String.to_integer(head)})
+  defp do_something_else(list), do: list
 end

5907a4f5aeb411d39f3c919d2c3fc557f655097f
Warren Jones <warren.jones@snyk.io>
2023-12-04 10:54:00 +0000

    feat: solution to day 4 part 2

--- aoc/lib/day4.ex
+++ aoc/lib/day4.ex
@@ -16,4 +16,26 @@ defmodule Day4 do
     end)
     |> Enum.sum()
   end
+
+  def part2(file) do
+    file
+    |> AOCUtil.lines!()
+    |> Enum.reverse()
+    |> Enum.reduce(%{}, fn line, acc -> 
+      [id, content | []] = String.split(line, ": ")
+      [_, id | []] = String.split(id)
+      id = String.to_integer(id)
+      [winners, candidates | []] = String.split(content, " | ")
+      winners = winners |> String.split() |> MapSet.new()
+      candidates = candidates |> String.split() |> MapSet.new()
+      matches = MapSet.intersection(winners, candidates) |> MapSet.to_list() |> length()
+      
+      Map.put(acc, id, case matches do
+        0 -> 1
+        n -> 1 + ((for x <- (id + 1)..(id + n), do: Map.get(acc, x)) |> Enum.sum())
+      end)
+    end)
+    |> Map.values()
+    |> Enum.sum()
+  end
 end

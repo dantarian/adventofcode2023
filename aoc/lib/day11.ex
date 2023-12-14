270bca542364c20f349e5e4e2397f7515bdfe81b
Warren Jones <warren.jones@snyk.io>
2023-12-14 18:47:28 +0000

    chore: reformat

--- aoc/lib/day11.ex
+++ aoc/lib/day11.ex
@@ -13,48 +13,76 @@ defmodule Day11 do
     distances(galaxies, empty_rows, empty_cols, expansion_factor - 1)
   end
 
-  defp parse(file), do:
-    file 
-    |> AOCUtil.lines!()
-    |> Enum.reject(fn x -> x == "" end)
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {line, y} -> 
-      line
-      |> String.to_charlist()
+  defp parse(file),
+    do:
+      file
+      |> AOCUtil.lines!()
+      |> Enum.reject(fn x -> x == "" end)
       |> Enum.with_index()
-      |> Enum.map(fn {c, x} ->
-        case c do
-          ?. -> nil
-          ?# -> {x, y}
-        end
+      |> Enum.flat_map(fn {line, y} ->
+        line
+        |> String.to_charlist()
+        |> Enum.with_index()
+        |> Enum.map(fn {c, x} ->
+          case c do
+            ?. -> nil
+            ?# -> {x, y}
+          end
+        end)
+        |> Enum.reject(fn x -> x == nil end)
       end)
-      |> Enum.reject(fn x -> x == nil end)
-    end)
- 
+
   defp find_empty(points) do
     {{x_min, _}, {x_max, _}} = points |> Enum.min_max_by(fn {x, _} -> x end)
     {{_, y_min}, {_, y_max}} = points |> Enum.min_max_by(fn {_, y} -> y end)
     used_cols = points |> Enum.map(fn {x, _} -> x end) |> MapSet.new()
     used_rows = points |> Enum.map(fn {_, y} -> y end) |> MapSet.new()
-    {y_min..y_max |> MapSet.new() |> MapSet.difference(used_rows), x_min..x_max |> MapSet.new() |> MapSet.difference(used_cols)}
+
+    {y_min..y_max |> MapSet.new() |> MapSet.difference(used_rows),
+     x_min..x_max |> MapSet.new() |> MapSet.difference(used_cols)}
   end
 
-  defp distances(galaxies, empty_rows, empty_cols, expansion_factor \\ 1), do: do_distances(galaxies, empty_rows, empty_cols, expansion_factor, 0)
+  defp distances(galaxies, empty_rows, empty_cols, expansion_factor \\ 1),
+    do: do_distances(galaxies, empty_rows, empty_cols, expansion_factor, 0)
 
   defp do_distances([], _, _, _, total), do: total
-  defp do_distances([point | rest], empty_rows, empty_cols, expansion_factor, total), do:
-    do_distances(rest, empty_rows, empty_cols, expansion_factor, total + point_distances(point, rest, empty_rows, empty_cols, expansion_factor))
 
-  defp point_distances(point, others, empty_rows, empty_cols, expansion_factor), do:
-    do_point_distances(point, others, empty_rows, empty_cols, expansion_factor, 0)
+  defp do_distances([point | rest], empty_rows, empty_cols, expansion_factor, total),
+    do:
+      do_distances(
+        rest,
+        empty_rows,
+        empty_cols,
+        expansion_factor,
+        total + point_distances(point, rest, empty_rows, empty_cols, expansion_factor)
+      )
+
+  defp point_distances(point, others, empty_rows, empty_cols, expansion_factor),
+    do: do_point_distances(point, others, empty_rows, empty_cols, expansion_factor, 0)
 
   defp do_point_distances(_, [], _, _, _, total), do: total
-  defp do_point_distances({x1, y1} = p1, [{x2, y2} = p2 | rest], empty_rows, empty_cols, expansion_factor, total) do
+
+  defp do_point_distances(
+         {x1, y1} = p1,
+         [{x2, y2} = p2 | rest],
+         empty_rows,
+         empty_cols,
+         expansion_factor,
+         total
+       ) do
     empty_x = count_empty(x1, x2, empty_cols)
     empty_y = count_empty(y1, y2, empty_rows)
-    do_point_distances(p1, rest, empty_rows, empty_cols, expansion_factor, total + AOCUtil.manhattan(p1, p2) + (empty_x * expansion_factor) + (empty_y * expansion_factor))
+
+    do_point_distances(
+      p1,
+      rest,
+      empty_rows,
+      empty_cols,
+      expansion_factor,
+      total + AOCUtil.manhattan(p1, p2) + empty_x * expansion_factor + empty_y * expansion_factor
+    )
   end
 
-  defp count_empty(a, b, known_empty), do:
-    min(a, b)..max(a, b) |> MapSet.new() |> MapSet.intersection(known_empty) |> MapSet.size()
+  defp count_empty(a, b, known_empty),
+    do: min(a, b)..max(a, b) |> MapSet.new() |> MapSet.intersection(known_empty) |> MapSet.size()
 end
--- aoc/lib/day13.ex
+++ aoc/lib/day13.ex
@@ -8,27 +8,34 @@ defmodule Day13 do
 
   def part2(file) do
     blocks = file |> parse()
-    h = blocks |> Enum.map(fn block -> block |> h_symmetry(&find_near_symmetry/1) end) |> Enum.sum()
-    v = blocks |> Enum.map(fn block -> block |> v_symmetry(&find_near_symmetry/1) end) |> Enum.sum()
+
+    h =
+      blocks |> Enum.map(fn block -> block |> h_symmetry(&find_near_symmetry/1) end) |> Enum.sum()
+
+    v =
+      blocks |> Enum.map(fn block -> block |> v_symmetry(&find_near_symmetry/1) end) |> Enum.sum()
+
     h + 100 * v
   end
 
   defp parse(file) do
     file
-    |> AOCUtil.blocks!() 
+    |> AOCUtil.blocks!()
     |> Enum.reject(fn x -> x == "" end)
-    |> Enum.map(fn block -> 
-      rows = block
-      |> String.split("\n")
-      |> Enum.with_index(1)
-      
-      map = rows
-      |> Enum.reduce(%{}, fn {row, y}, acc ->
-        row
+    |> Enum.map(fn block ->
+      rows =
+        block
+        |> String.split("\n")
+        |> Enum.with_index(1)
+
+      map =
+        rows
+        |> Enum.reduce(%{}, fn {row, y}, acc ->
+          row
           |> String.to_charlist()
           |> Enum.with_index(1)
           |> Enum.reduce(acc, fn {c, x}, acc -> Map.put(acc, {x, y}, c) end)
-      end)
+        end)
 
       %{
         y_max: map |> Map.keys() |> Enum.map(fn {_, y} -> y end) |> Enum.max(),
@@ -39,21 +46,25 @@ defmodule Day13 do
   end
 
   defp h_symmetry(%{y_max: y_max, x_max: x_max, map: %{} = map}, finder \\ &find_symmetry/1) do
-    cols = for x <- 1..x_max, into: [] do
-      for y <- 1..y_max, into: [] do
-        map |> Map.get({x, y})
-      end |> List.to_string()
-    end
+    cols =
+      for x <- 1..x_max, into: [] do
+        for y <- 1..y_max, into: [] do
+          map |> Map.get({x, y})
+        end
+        |> List.to_string()
+      end
 
     finder.(cols)
   end
 
   defp v_symmetry(%{y_max: y_max, x_max: x_max, map: %{} = map}, finder \\ &find_symmetry/1) do
-    rows = for y <- 1..y_max, into: [] do
-      for x <- 1..x_max, into: [] do
-        map |> Map.get({x, y})
-      end |> List.to_string()
-    end
+    rows =
+      for y <- 1..y_max, into: [] do
+        for x <- 1..x_max, into: [] do
+          map |> Map.get({x, y})
+        end
+        |> List.to_string()
+      end
 
     finder.(rows)
   end
@@ -61,20 +72,31 @@ defmodule Day13 do
   defp find_symmetry(list), do: do_find_symmetry(list, [])
 
   defp do_find_symmetry([], _), do: 0
+
   defp do_find_symmetry([next | rest], [last | other_previous] = previous) when next == last do
-    if confirm_symmetry(rest, other_previous), do: length(previous), else: do_find_symmetry(rest, [next | previous])
+    if confirm_symmetry(rest, other_previous),
+      do: length(previous),
+      else: do_find_symmetry(rest, [next | previous])
   end
+
   defp do_find_symmetry([next | rest], previous), do: do_find_symmetry(rest, [next | previous])
 
-  defp confirm_symmetry(remaining, previous), do: Enum.zip(remaining, previous) |> Enum.all?(fn {a, b} -> a == b end)
+  defp confirm_symmetry(remaining, previous),
+    do: Enum.zip(remaining, previous) |> Enum.all?(fn {a, b} -> a == b end)
 
   defp find_near_symmetry(list), do: do_find_near_symmetry(list, [])
   defp do_find_near_symmetry([], _), do: 0
   defp do_find_near_symmetry([next | rest], []), do: do_find_near_symmetry(rest, [next])
+
   defp do_find_near_symmetry([next | rest] = list, previous) do
-    if check_near_symmetry(list, previous), do: length(previous), else: do_find_near_symmetry(rest, [next | previous])
+    if check_near_symmetry(list, previous),
+      do: length(previous),
+      else: do_find_near_symmetry(rest, [next | previous])
   end
 
-  defp check_near_symmetry(list, previous), do:
-    (Enum.zip(list, previous) |> Enum.map(fn {x, y} -> Levenshtein.distance(x, y) end) |> Enum.sum()) == 1
+  defp check_near_symmetry(list, previous),
+    do:
+      Enum.zip(list, previous)
+      |> Enum.map(fn {x, y} -> Levenshtein.distance(x, y) end)
+      |> Enum.sum() == 1
 end
